e724ddae003552cf9e90cab120e1cbea1f929aec
Deise Domingues <dominguesdeise@gmail.com>
2025-12-03 20:04:41 -0300

    fix: corrige porta para o Render

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ import cors from "cors";
 import routes from "./routes/index.js";
 import pool from "./database/connection.js";
 import musicoRoutes from "./routes/musicoRoutes.js";
-import avaliacaoRoutes from "./routes/avaliacaoRoutes.js"; 
+import avaliacaoRoutes from "./routes/avaliacaoRoutes.js";
 
 const app = express();
-const PORT = 3000;
+
+// Porta para o Render
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// Teste de conexão com MySQL
 pool.getConnection()
   .then(() => console.log("✅ Conectado ao MySQL com sucesso!"))
   .catch(err => console.error("❌ Erro ao conectar ao MySQL:", err));
 
+// Rotas
 app.use(routes);
 app.use(musicoRoutes);
 app.use(avaliacaoRoutes);
 
-app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+// Servidor
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando na porta ${port}`);
+});
